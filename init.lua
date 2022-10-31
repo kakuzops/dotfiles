ad5b8c366f780f914a7d645e6843fc6511807950
kakuzops <kakuzops@gmail.com>
2022-10-31 20:25:12 -0300

    init files

---
+++ init.lua
@@ -0,0 +1,8 @@
+require('lspconfig').pyright.setup{}
+require('settings')
+ require('mappings')
+ require('packer-config')
+ require('colorschemes-config.onedark')
+ require('nvim-tree-config')
+ require('lsp-config.language-servers')
+ require('lsp-config.nvim-cmp')
